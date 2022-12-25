aff9b68670e1feb6807881c7df1011c1c0c0b7a4
Srini09git <srinimohand@gmail.com>
2022-12-25 21:02:32 +0530

    new

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import { OverviewChart } from './OverviewChart';
 import { ProjectsList } from './Projects';
 import { StatusCardsList } from './StatusCards';
 
-
-//admin dashboard - day4 task
-//preview link - https://startbootstrap.com/previews/sb-admin-2
-
 function App() {
   return (
     <div className="App">
