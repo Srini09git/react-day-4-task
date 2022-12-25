6fea85ee1561a28a8892f40d34385308840e23b4
Srini09git <srinimohand@gmail.com>
2022-12-25 21:00:20 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { ColoredCardsList } from './ColoredCards';
+import { OverviewChart } from './OverviewChart';
+import { ProjectsList } from './Projects';
+import { StatusCardsList } from './StatusCards';
+
+
+//admin dashboard - day4 task
+//preview link - https://startbootstrap.com/previews/sb-admin-2
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='title'>Dashboard</h1>
+      <StatusCardsList />
+      <OverviewChart />
+      <ProjectsList />
+      <ColoredCardsList />
     </div>
   );
 }
